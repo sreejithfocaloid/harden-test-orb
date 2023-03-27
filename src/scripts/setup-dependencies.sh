34c50d046457d4f7a0b0c32176fc9b7865ced3f2
Sreejith <sreejiths@focaloid.com>
2023-03-27 12:27:12 +0530

    test orb setup

--- src/scripts/setup-dependencies.sh
+++ src/scripts/setup-dependencies.sh
@@ -10,7 +10,7 @@ sudo apt install -qq -y libxml2-utils
 # echo "Adding ${HOME}/.slim/bin"
 # export PATH="${HOME}/.slim/bin:$PATH"
 # echo "${HOME}/.slim/bin" >>$GITHUB_PATH
-slim config gen --save --token ${PORTAL0_API_TOKEN} --org ${PORTAL0_ORGANIZATION_ID}
+slim config gen --save --token "${PORTAL0_API_TOKEN}" --org "${PORTAL0_ORGANIZATION_ID}"
 # echo "token: ${PORTAL0_API_TOKEN}"> ~/.config/slim/config
 # echo "default_org: ${PORTAL0_ORGANIZATION_ID}">> ~/.config/slim/config
 # echo "endpoint: https://api.dev-jb.saas.getslim.ai">> ~/.config/slim/config
