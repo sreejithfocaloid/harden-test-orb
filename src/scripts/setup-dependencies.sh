56c3bdb46d0985d518baab3ad1e35e1c705ef68a
Sreejith <sreejiths@focaloid.com>
2023-04-03 15:31:56 +0530

    inst test

--- src/scripts/setup-dependencies.sh
+++ src/scripts/setup-dependencies.sh
@@ -10,8 +10,8 @@ sudo apt install -qq -y libxml2-utils
 # echo "Adding ${HOME}/.slim/bin"
 # export PATH="${HOME}/.slim/bin:$PATH"
 
-echo "export PATH=$HOME/.slim/bin:$PATH" >> "${HOME}"/.bashrc
-source "${HOME}"/.bashrc
+echo "export PATH=~/.slim/bin:$PATH" >> ~/.bashrc
+source ~/.bashrc
 # echo "${HOME}/.slim/bin" >>$GITHUB_PATH
 slim config gen --save --token "${PORTAL0_API_TOKEN}" --org "${PORTAL0_ORGANIZATION_ID}"
 # echo "token: ${PORTAL0_API_TOKEN}"> ~/.config/slim/config
