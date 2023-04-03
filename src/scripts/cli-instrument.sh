5adac31f1dbaceee5500d4ceccbd7b7f1db4494d
Sreejith <sreejiths@focaloid.com>
2023-04-03 16:03:39 +0530

    inst test

--- src/scripts/cli-instrument.sh
+++ src/scripts/cli-instrument.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 export PATH="${HOME}/.slim/bin:$PATH"
 echo Hello "Instrument"
-echo "$ABC"
+echo "${ABC}"
 SOURCE_CONNECTOR_ID="dockerhub.public"
 PROJECT_IMAGE_INSTRUMENTED="sreejithfocaloid/node-example:latest.instrumented"
 PROJECT_IMAGE_SLIMMED="sreejithfocaloid/node-example:latest.slimxx"
