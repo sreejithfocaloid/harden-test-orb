06ce3ad8b97580c78513736c1c6ebb5e4b50bd47
Sreejith <sreejiths@focaloid.com>
2023-04-03 16:56:32 +0530

    inst test

--- src/scripts/run.sh
+++ src/scripts/run.sh
@@ -2,4 +2,4 @@
 IMAGE="sreejithfocaloid/node-example:latest.instrumented"
 docker pull $IMAGE
 
-eval "docker run --name my-orb-project 9001:9001 $IMAGE"
+eval "docker run --name my-orb-project -p 9001:9001 $IMAGE"
