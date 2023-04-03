0d30d7299aed78ddfabb714911e189454684628d
Sreejith <sreejiths@focaloid.com>
2023-04-03 17:02:20 +0530

    inst test

--- src/scripts/run.sh
+++ src/scripts/run.sh
@@ -2,4 +2,4 @@
 IMAGE="sreejithfocaloid/node-example:latest.instrumented"
 docker pull $IMAGE
 
-eval "docker run --cap-add ALL --name my-orb-project -p 9001:9001 $IMAGE"
+eval "docker run --cap-add ALL --user root --name my-orb-project -p 9001:9001 $IMAGE"
