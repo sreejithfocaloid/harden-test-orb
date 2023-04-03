e89c4335054f01ece7ac8060926fd48756176dcb
Sreejith <sreejiths@focaloid.com>
2023-04-03 17:08:50 +0530

    inst test

--- src/scripts/cleanup.sh
+++ src/scripts/cleanup.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 PROJECT_CONTAINER_NAME="my-orb-project"
 docker stop "$PROJECT_CONTAINER_NAME"
+docker logs "$PROJECT_CONTAINER_NAME" 
 docker wait "$PROJECT_CONTAINER_NAME"
 docker rm --force "$PROJECT_CONTAINER_NAME"
