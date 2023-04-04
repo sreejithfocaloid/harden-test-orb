681a015678d4d37ad86576ea6fa99fb4a61f0e97
Sreejith <sreejiths@focaloid.com>
2023-04-04 14:06:03 +0530

    Testing orb jobs

--- src/scripts/cli-harden.sh
+++ src/scripts/cli-harden.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
-echo "$CIRCLE_ARTIFACTS"
-curl -O "$CIRCLE_ARTIFACTS/my_var.txt"
 WORKFLOW_ID=$(cat my_var.txt)
 echo "Hardening workflow: $WORKFLOW_ID"
 log_output=$(slim harden --id "$WORKFLOW_ID" 2>&1 | tee /dev/stderr)
